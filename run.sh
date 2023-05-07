ebd3af65231f2f8a32856a34fab95d88e3c5ceed
jnko266 <33516559+jnko266@users.noreply.github.com>
2023-05-08 00:31:02 +0100

    aims to resolve #1

--- run.sh
+++ run.sh
@@ -71,8 +71,7 @@ sudo openssl req -new -key /etc/apache2/ssl/server/server.key -out /etc/apache2/
 sudo openssl x509 -req -in /etc/apache2/ssl/server/server.csr -CA /etc/apache2/ssl/rootCA/rootCA.crt -CAkey /etc/apache2/ssl/rootCA/rootCA.key -CAcreateserial -out /etc/apache2/ssl/server/server.crt -days 10 -sha256
 
 # Create a virtual host configuration file
-sudo bash -c 'cat > /etc/apache2/sites-available/000-custom-ssl.conf << EOL
-<VirtualHost *:80>
+sudo sh -c "echo '<VirtualHost *:80>
 	ServerAdmin ${SERVER_ADMIN}
 	DocumentRoot /var/www/html
 	ErrorLog ${APACHE_LOG_DIR}/error.log
@@ -91,7 +90,7 @@ sudo bash -c 'cat > /etc/apache2/sites-available/000-custom-ssl.conf << EOL
 		SSLEngine on
 		SSLCertificateFile /etc/apache2/ssl/server/server.crt
 		SSLCertificateKeyFile /etc/apache2/ssl/server/server.key
-		<FilesMatch "\.(cgi|shtml|phtml|php)$">
+		<FilesMatch \"\.(cgi|shtml|phtml|php)$\">
 			SSLOptions +StdEnvVars
 		</FilesMatch>
 		<Directory /usr/lib/cgi-bin>
@@ -103,8 +102,7 @@ sudo bash -c 'cat > /etc/apache2/sites-available/000-custom-ssl.conf << EOL
 			Require all granted
 		</Directory>
 	</VirtualHost>
-</IfModule>
-EOL'
+</IfModule>' > /etc/apache2/sites-available/000-custom-ssl.conf"
 
 # Enable the custom virtual host
 sudo a2ensite 000-custom-ssl
@@ -115,30 +113,29 @@ sudo a2dissite 000-default
 # Restart Apache to apply changes
 sudo systemctl restart apache2
 
-#Create a script to renew the server certificate
-sudo bash -c 'cat > /usr/local/bin/renew_cert.sh << EOL
-#!/bin/bash
+# Create a script to renew the server certificate
+sudo sh -c "echo '#!/bin/bash
 
 # Set the threshold (in days) for renewing the certificate
 THRESHOLD=2
 
 # Get the expiration date of the current certificate
-EXPIRATION_DATE=$(openssl x509 -in /etc/apache2/ssl/server/server.crt -enddate -noout | cut -d "=" -f 2)
+EXPIRATION_DATE=\$(openssl x509 -in /etc/apache2/ssl/server/server.crt -enddate -noout | cut -d \"=\" -f 2)
 
 # Convert the expiration date to the number of seconds since 1970-01-01 00:00:00 UTC
-EXPIRATION_DATE_SEC=$(date -d "${EXPIRATION_DATE}" +%s)
+EXPIRATION_DATE_SEC=\$(date -d \"\${EXPIRATION_DATE}\" +%s)
 
 # Get the current date and time in seconds
-CURRENT_DATE_SEC=$(date +%s)
+CURRENT_DATE_SEC=\$(date +%s)
 
 # Calculate the time difference between the expiration date and the current date (in days)
-TIME_DIFF_DAYS=$(( (EXPIRATION_DATE_SEC - CURRENT_DATE_SEC) / 86400 ))
+TIME_DIFF_DAYS=\$(( (EXPIRATION_DATE_SEC - CURRENT_DATE_SEC) / 86400 ))
 
 # Renew the certificate only if it is close to expiration
-if [ ${TIME_DIFF_DAYS} -le ${THRESHOLD} ]; then
+if [ \${TIME_DIFF_DAYS} -le \${THRESHOLD} ]; then
 	# Generate server key and CSR
 	sudo openssl genrsa -out /etc/apache2/ssl/server/server.key 2048
-	sudo openssl req -new -key /etc/apache2/ssl/server/server.key -out /etc/apache2/ssl/server/server.csr -subj "/C=${COUNTRY}/ST=${STATE}/L=${CITY}/O=${ORG}/OU=${OU}/CN=${CN}"
+	sudo openssl req -new -key /etc/apache2/ssl/server/server.key -out /etc/apache2/ssl/server/server.csr -subj \"/C=${COUNTRY}/ST=${STATE}/L=${CITY}/O=${ORG}/OU=${OU}/CN=${CN}\"
 
 	# Sign the server certificate with the root CA for 10 days
 	sudo openssl x509 -req -in /etc/apache2/ssl/server/server.csr -CA /etc/apache2/ssl/rootCA/rootCA.crt -CAkey /etc/apache2/ssl/rootCA/rootCA.key -CAcreateserial -out /etc/apache2/ssl/server/server.crt -days 10 -sha256
@@ -146,7 +143,8 @@ if [ ${TIME_DIFF_DAYS} -le ${THRESHOLD} ]; then
 	# Restart Apache to apply changes
 	sudo systemctl restart apache2
 fi
-EOL'
+' > /usr/local/bin/renew_cert.sh"
+
 
 # Make the script executable
 sudo chmod +x /usr/local/bin/renew_cert.sh
